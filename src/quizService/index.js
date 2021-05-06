4dacb90d5856cb8197898879fc3020b077f7a5da
Michael Zeta <michael@a2zeta.com>
2021-05-05 18:43:19 -0700

    Removed unnamed component error

--- src/quizService/index.js
+++ src/quizService/index.js
@@ -372,5 +372,8 @@ const qBank = [
   },
 ]
 
-export default (n = 5) =>
+const quizService = (n = 5) => {
   Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n))
+}
+
+export default quizService
